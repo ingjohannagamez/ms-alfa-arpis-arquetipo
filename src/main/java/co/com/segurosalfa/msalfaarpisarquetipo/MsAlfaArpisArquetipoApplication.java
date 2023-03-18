80c4829e76a6c1dd4c6242d0308f469123976646
Johann Andres Agamez Ferres <ingjohannagamez@gmail.com>
2023-03-18 15:10:28 -0500

    first commit

---
+++ src/main/java/co/com/segurosalfa/msalfaarpisarquetipo/MsAlfaArpisArquetipoApplication.java
@@ -0,0 +1,13 @@
+package co.com.segurosalfa.msalfaarpisarquetipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAlfaArpisArquetipoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAlfaArpisArquetipoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/segurosalfa/msalfaarpisarquetipo/MsAlfaArpisArquetipoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.segurosalfa.msalfaarpisarquetipo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAlfaArpisArquetipoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
